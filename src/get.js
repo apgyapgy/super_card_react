83278719c15e37d7c3f6c2afd8f8d1d8f225e763
apgyapgy <2648226617@qq.com>
2018-02-23 17:11:20 +0800

    axios跨域问题
    暂时放弃fetch用axios请求数据，现在纠结于跨域问题。

--- src/get.js
+++ src/get.js
@@ -1,21 +1,40 @@
-import 'whatwg-fetch';
+//import 'whatwg-fetch';
+
+import axios from  'axios';
 export function get(url){
-	var isDebug = true,preUrl='';
+	var isDebug = false,preUrl='';
 	if(isDebug){
 		preUrl = 'https://dswx-test.fuiou.com/o2o/';
 	}else{
 		preUrl = 'https://chi.fuiou.com/';
 	}
-	fetch(preUrl + url,{
+	axios.get(preUrl + url)
+	.then((res) => {
+		console.log("res:",res);
+	}).catch((err) => {
+		console.error("err:",err);
+	})
+	/*fetch(preUrl + url,{
+	//fetch('./App.js',{
 		method:'get',
 		mode:'no-cors',
 		credentials: 'include',
 		headers: {
 		    "Content-Type": "application/x-www-form-urlencoded"
 		}
-	}).then(res => {
-        console.log("res:",res);
-    }).catch(e=>{console.log(e);})
+	}).then(response => {
+		console.log("res:",response);
+	    if (response.ok) {
+	      	return response.json()
+	    } else {
+	      	return Promise.reject({
+        		status: response.status,
+        		statusText: response.statusText
+      		})
+	    }
+  	})
+  	.then(data => console.log('data is', data))
+  	.catch(error => console.log('error is', error));*/
 }
 
 /*return new Promise((resolve,reject) => {
